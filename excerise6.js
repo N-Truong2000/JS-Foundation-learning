9455a365de4634bd4425bb067f80897f7524e19a
Nhat_Truong <tranhuunhattruong2000@gmail.com>
2023-12-04 23:29:45 +0700

    excerise 6

---
+++ excerise6.js
@@ -0,0 +1,31 @@
+let countdownA, countupA;
+
+function countdown(ss, callback) {
+  countdownA = setInterval(() => {
+    console.log(ss);
+    ss--;
+    if (ss < 0) {
+      callback();
+    }
+  }, 1000);
+}
+
+function countup(ss, callback) {
+  let i = 0;
+  countupA = setInterval(() => {
+    console.log(i);
+    i++;
+    if (i > ss) {
+      callback();
+    }
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(countdownA || countupA);
+  console.log("stop success");
+}
+
+//countdown(5, stop);
+//countup(4,stop)
+
--- test.js
+++ test.js
@@ -1,4 +1,23 @@
-var a=[1,2,3,4,5];
-var b=[6,7,8,9,10]
-const j = Math.floor(Math.random() * 13);
-console.log(j);
+// Mảng gốc
+// let boBai = [2, 4, 6, 8, 10];
+
+// // Gán giá trị i và j cho 1 và 3 để hoán đổi giá trị của boBai[1] và boBai[3]
+// const i = 1;
+// const j = 3;
+
+// // In ra mảng gốc trước khi hoán đổi
+// console.log("Mảng gốc trước khi hoán đổi: ", boBai); // Output: [2, 4, 6, 8, 10]
+
+// // Hoán đổi giá trị của boBai[i] và boBai[j]
+// [boBai[i], boBai[j]] =[boBai[j], boBai[i]];
+
+// // In ra mảng sau khi hoán đổi
+// console.log("Mảng sau khi hoán đổi: ", boBai); // Output: [2, 8, 6, 4, 10]
+
+const colors = ['red', 'green', 'blue'];
+
+const [secondColor, thirdColor] = colors;
+
+console.log(); // Output: 'red'
+console.log(secondColor); // Output: 'green'
+console.log(thirdColor);
